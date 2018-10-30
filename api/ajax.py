a72727a78bdfe1486bd6b8cfe4a036cf1ae3bc2c
Gawaboumga <yourihubaut@hotmail.com>
2018-10-30 18:23:41 +0100

    Of course, sort none() query is not sorted ...

--- api/ajax.py
+++ api/ajax.py
@@ -7,39 +7,38 @@ class FunctionAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
         # Don't forget to filter out results depending on the visitor !
         if not self.request.user.is_authenticated:
-            return Function.objects.none()
+            return Function.objects.none().order_by('pk')
 
         qs = Function.objects.all()
 
         if self.q:
             qs = qs.filter(function__istartswith=self.q)
 
-        return qs
+        return qs.order_by('pk')
 
 
 class NameAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
         # Don't forget to filter out results depending on the visitor !
         if not self.request.user.is_authenticated:
-            return Name.objects.none()
+            return Name.objects.none().order_by('name')
 
         qs = Name.objects.all()
 
         if self.q:
             qs = qs.filter(name__istartswith=self.q)
 
-        return qs
+        return qs.order_by('name')
 
 
 class TagAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
         # Don't forget to filter out results depending on the visitor !
         if not self.request.user.is_authenticated:
-            return Tag.objects.none()
+            return Tag.objects.none().order_by('tag')
 
         qs = Tag.objects.all()
-
         if self.q:
             qs = qs.filter(tag__istartswith=self.q)
 
-        return qs
+        return qs.order_by('tag')
